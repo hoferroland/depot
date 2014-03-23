f5465926e75d169c6f6a7166e2ae25d738f404ad
hoferroland <hofer.roland@bluewin.ch>
2014-03-23 12:03:17 +0100

    aufgaben validation und testing

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,18 @@
 class Product < ActiveRecord::Base
-end
+	
+	
+validates :title, :description, :image_url, presence: true
+#validates :price, numericality: {greater_than_or_equal_to: 0.01}
+validates :price, numericality: {greater_than_or_equal_to: 0.05}
+validates :price, format: {
+	with: 		%r{\A[\d]*\.[\d][05]\z},
+	#with: 		%r{^\d*\.\d[5]?$},
+	message:	'Betrag muss auf 5 Rappen gerundet sein!'
+}
+validates :title, uniqueness: true
+validates :image_url, allow_blank: true, format: {
+	with:		%r{\.(gif|jpg|png)\Z}i,
+	message: 	'Es muss sich um eine Bild-Url im Format GIF, JPG oder PNG handeln!'
+}
+
+end	
