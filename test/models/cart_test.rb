daeb1244b0e3fdbb71a1bc3f6cf3c33d3b956753
hoferroland <hofer.roland@bluewin.ch>
2014-04-29 18:43:03 +0200

    Aufg Kap 10

--- test/models/cart_test.rb
+++ test/models/cart_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class CartTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "if you add a product it has a quantity of 1" do
+    product_id = products(:ruby).id
+    cart = Cart.create
+    line_item = cart.add_product(product_id)
+
+    assert_equal product_id, line_item.product_id
+    assert_equal 1, line_item.quantity
+  end
+
+  test "if you add a product a second one it has quantity of 2" do
+    product_id = products(:ruby).id
+    cart = Cart.create
+    cart.add_product(product_id)
+    cart.save!
+    line_item = cart.add_product(product_id)
+
+    assert_equal product_id, line_item.product_id
+    assert_equal 2, line_item.quantity
+  end
 end
